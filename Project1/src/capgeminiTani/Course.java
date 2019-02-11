fc1eb8e0a06310783122b0956d454536b1282567
komal1906 <47099117+komal1906@users.noreply.github.com>
2019-02-11 23:07:32 +0530

    Add files via upload

---
+++ Project1/src/capgeminiTani/Course.java
@@ -0,0 +1,26 @@
+package capgeminiTani;
+
+public class Course {
+private int courseid;
+private String courseName;
+public int getCourseId()
+{
+	return courseid;
+}
+
+public void setCourseId(int courseid)
+{		
+	this.courseid=courseid;
+}
+
+public String getCourseName()
+{
+	return courseName;
+}
+public void setCourseName(String courseName)
+{
+ this.courseName=courseName;
+}
+
+	
+}
